14aeadca230bc4368f6b6afcdacae0df02722f57
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-04-06 14:30:59 -0500

    file header shell.h

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+/* prototypes owner */
+char *_getlines(void);
+int _strcmp(char *s1, char *s2);
+char **_strtok(char *argv);
+int _execev(char **argvs);
+char *_strcat(char *dest, char *src);
+#endif
