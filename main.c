07e45f04cf211e7e0099a86ad6cd8b26c6896d3f
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-04-10 13:49:00 -0500

    recovery main file

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
 #include "shell.h"
 
-/**
- * main - prints the environment
- *
- * Return: Always 0.
- */
-int main(int ac, char **av, char **env)
+int main (int ac, char **av, char **env)
 {
-    (void)ac;
-    (void)av;
+	(void) ac;
+	(void) av;
+	(void) env;
 
-    char **test = get_path(env);
-    char *p;
-	/* pointer for erase **string */
-    char **ptr = test;
-	/* pointer for erase *string */ 
-    char **ptr2 = test;
+    int status = 0;
 
-    while (*test != NULL)
-    {
-        p = *test;
-        printf("%s\n", p);
-        test++;
+	initiate_shell();
+    while (status != -1) {
+	
+		/* get the input the command line */
+		char *argv = _getlines();
+		/* split the input line with strtok funtion and return 2d ponter */
+		if (argv != NULL)
+		{
+			char **split_argv = _strtok(argv);
+			/* check split_argv[0] for there is an bult-in funtion */
+			/* -----CALL FUNTION FOR BULT-IN FUNTIONS----- */
+			if (strcmp(split_argv[0],"exit") == 0)
+			{
+				exit(EXIT_SUCCESS);
+			}
+
+			if (strcmp(split_argv[0],"clear") == 0)
+			{
+				CLEAR_SCREEN;
+			}
+			/* call the funtion for executing external funtions with exec */
+			status = _execev(split_argv);
+			/* free memory */
+			free(argv);
+			free_grid(split_argv);
+		}
     }
-
-	/* free the memory var **ponter */
-    while (*ptr != NULL)
-    {
-      p = *ptr;
-      free(p);
-      ptr++;
-    }
-    free(ptr2);
-    return (0);
+    return 0;
 }
